a2f0c069c5bb8cb364b3cbdd4aa07c8ed5361b16
Igor Braga <igorbgalvan@hotmail.com>
2020-10-05 22:11:44 -0400

    profile photo added

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -37,6 +37,7 @@ class User extends Entity
         'position' => true,
         'phone' => true,
         'role_id' => true,
+        'profile_picture' => true,
         'confirmation' => true,
         'created' => true,
         'modified' => true,
