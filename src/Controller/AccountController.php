ceeff94449be2a16dd08ff1f152b17039af47f2b
Igor Braga <igorbgalvan@hotmail.com>
2020-10-06 23:07:14 -0400

    email recover

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -18,7 +18,113 @@ class AccountController extends AppController
     public function beforeFilter(Event $event)
     {
         parent::beforeFilter($event);
-        $this->Auth->allow(['lostAccount']);
+        $this->Auth->allow(['lostAccount', 'verifyToken']);
+    }
+
+    public function verifyToken()
+    {
+        if ($this->request->is('post')) {
+            $Tokens = TableRegistry::getTableLocator()->get('Tokens');
+            $Users = TableRegistry::getTableLocator()->get('Users');
+
+            $data = $this->request->getData();
+            $id = $data['id'];
+            $tokenCode = $data['tokenCode'];
+
+            $user = $Users->get($id);
+
+
+            if ($user) {
+                $token = $Tokens->find('all', [
+                    'conditions' => ['user_id' => $id]
+                ])->first();
+
+
+                if ($token) {
+
+                    var_dump($token->token);
+                    var_dump(password_hash($tokenCode, PASSWORD_DEFAULT));
+                    die();
+
+                    if (password_verify($tokenCode, $token->token)) {
+                        $tokenTime = $token->expiration + 1800;
+
+
+                        if($tokenTime >= time())
+                        {
+                            $this->response->statusCode('200');
+                            $data = [
+                                'message' => 'token valid.'
+                            ];
+                        }
+                        else{
+                            $this->response->statusCode('400');
+                            $data = [
+                                'message' => 'token expired.',
+                                'error' => true
+                            ];
+                        }
+                    }
+                    else{
+                        $this->response->statusCode('400');
+                        $data = [
+                            'message' => 'token not valid.',
+                            'error' => true
+                        ];
+                    }
+
+                } else {
+                    $this->response->statusCode('400');
+                    $data = [
+                        'message' => 'token not valid.',
+                        'error' => true
+                    ];
+                }
+            } else {
+                $this->response->statusCode('400');
+                $data = [
+                    'message' => 'user not valid.',
+                    'error' => true
+                ];
+            }
+        }
+    }
+
+    public function changePass($token = null)
+    {
+        $Users = TableRegistry::getTableLocator()->get('Users');
+
+        if ($this->request->is('post')) {
+            $data = $this->request->getData();
+
+            $account = $Users->get($account_id);
+
+            $id = $data['id'];
+            $tokenCode = $data['tokenCode'];
+            $account_id = $data['tokenUser'];
+
+            $account = $Users->get($account_id);
+
+            if ($data['password'] != $data["confirm-password"]) {
+                $error = "As senhas não conferem!";
+                $this->set(compact('error', 'account_id'));
+                return;
+            }
+
+            $account->password = $data['password'] . $account->secret;
+            $account->authToken = '';
+
+            if ($Accounts->save($account)) {
+                $this->Flash->success(__('A senha foi alterada com sucesso!'));
+                return $this->redirect(['controller' => '/']);
+            }
+        }
+
+        if ($token == null || $token == '') {
+            die();
+            $this->Flash->error(__('Link Inválido!'));
+            return $this->redirect(['controller' => '/']);
+        }
     }
     public function lostAccount()
     {
@@ -35,7 +141,7 @@ class AccountController extends AppController
                 $token = $Tokens->newEntity();
 
                 $tokenCode = $this->generateToken(24);
-                $token->token = password_hash($token, PASSWORD_DEFAULT);
+                $token->token = password_hash($tokenCode, PASSWORD_DEFAULT);
                 $token->user_id = $user->id;
                 $token->expiration = time();
 
@@ -59,9 +165,7 @@ class AccountController extends AppController
                 $this->response->statusCode('400');
                 $data = ['message' => 'Email not valid.'];
             }
-        }
-        else
-        {
+        } else {
             $this->response->statusCode('400');
             $data = ['message' => 'Request needs to be post'];
         }
--- src/Template/Email/html/pass_recover.ctp
+++ src/Template/Email/html/pass_recover.ctp
@@ -2,6 +2,6 @@
 <p>Para mudar a senha da sua conta, acesse o link:</p>
 
 <p>E-mail: <?= $account_email ?></p>
-<p>Senha: <a href="localhost/users/change-pass/<?= $account_id . '.' . $confirm_email_token?>">
+<p>Senha: <a href="localhost/p05-ger-pericial/account/change-pass/<?= $account_id . '.' . $confirm_email_token?>">
   Clique aqui para alterar sua senha!
 </a></p>
