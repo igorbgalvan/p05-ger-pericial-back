a6e75b3c705423afcdbcd96e77898b0434c5c670
Igor Braga <igorbgalvan@hotmail.com>
2020-11-09 11:13:11 -0500

    Request and reports updated

--- src/Model/Table/ReportsTable.php
+++ src/Model/Table/ReportsTable.php
@@ -102,7 +102,6 @@ class ReportsTable extends Table
      */
     public function buildRules(RulesChecker $rules)
     {
-        $rules->add($rules->existsIn(['report_id'], 'Reports'));
         $rules->add($rules->existsIn(['user_id'], 'Users'));
         $rules->add($rules->existsIn(['request_id'], 'Requests'));
 
